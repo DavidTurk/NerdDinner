4ba96465e0115bd91d5f9358331d4f2191030268
David Turk <dturk@paraport.com>
2016-02-23 14:28:36 -0800

    Added functions to Home controller and added views.

--- NerdDinner/Controllers/HomeController.cs
+++ NerdDinner/Controllers/HomeController.cs
@@ -1,16 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using NerdDinner.Models;
 
 namespace NerdDinner.Controllers
 {
     public class HomeController : Controller
     {
+        private NerdDinners db = new NerdDinners();
+
+        // GET: Dinners
         public ActionResult Index()
         {
+            return View(db.Dinners.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dinner dinner = db.Dinners.Find(id);
+            if (dinner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dinner);
+        }
+
+        //
+        // GET: /Home/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //
+        //POST: /Home/Create
+        [HttpPost]
+        public ActionResult Create(Dinner dinner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dinners.Add(dinner);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(dinner);
+        }
+
+        // GET: Dinners/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dinner dinner = db.Dinners.Find(id);
+            if (dinner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dinner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Dinner dinner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dinner).State = (System.Data.Entity.EntityState)EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dinner);
+        }
+
+        // GET: Dinners/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dinner dinner = db.Dinners.Find(id);
+            if (dinner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dinner);
+        }
+
+        // POST: Dinners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Dinner dinner = db.Dinners.Find(id);
+            db.Dinners.Remove(dinner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
@@ -25,6 +121,14 @@ namespace NerdDinner.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ NerdDinner/Migrations/201602232159184_AddedViews.cs
@@ -0,0 +1,16 @@
+namespace NerdDinner.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedViews : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
