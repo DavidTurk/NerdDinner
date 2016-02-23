4084fb87549802b6b76d6729e2f69da2bd0fb074
David Turk <dturk@paraport.com>
2016-02-23 15:35:12 -0800

    Added dinnerrepository functionality

--- NerdDinner/Models/DinnerRepository.cs
+++ NerdDinner/Models/DinnerRepository.cs
@@ -7,5 +7,43 @@ namespace NerdDinner.Models
 {
     public class DinnerRepository
     {
+        private NerdDinners db = new NerdDinners();
+
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return db.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                where dinner.EventDate > DateTime.Now
+                orderby dinner.EventDate
+                select dinner;
+        }
+
+        public Dinner GetDinner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        public void Add(Dinner dinner)
+        {
+            db.Dinners.Add(dinner);
+        }
+
+        public void Delete(Dinner dinner)
+        {
+            foreach (RSVP rsvp in dinner.RSVPs)
+            {
+                db.RSVPs.Remove(rsvp);
+            }
+            db.Dinners.Remove(dinner);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
     }
 }
