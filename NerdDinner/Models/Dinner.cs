52245b986f26e4e2938707f62de3eaaf758ad94d
David Turk <dturk@paraport.com>
2016-02-23 12:24:34 -0800

    Created project and initial models

---
+++ NerdDinner/Models/Dinner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public class Dinner
+    {
+        public int DinnerID { get; set; }
+        public string Title { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Description { get; set; }
+        public string HostedBy { get; set; }
+        public string ContactPhone { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public virtual  ICollection<RSVP> RSVPs { get; set; } 
+    }
+}
