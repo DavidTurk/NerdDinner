ceb5ca8efb8de549082794fee3c55cfc51e41d76
David Turk <dturk@paraport.com>
2016-03-29 09:39:33 -0700

    Added jQuery scripts for ajax

--- NerdDinner/Controllers/RSVPController.cs
+++ NerdDinner/Controllers/RSVPController.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NerdDinner.Models;
+using PagedList;
 
 namespace NerdDinner.Controllers
 {
     public class RSVPController : Controller
     {
-       
+        private DinnerRepository dinnerRepository = new DinnerRepository();
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Register(int id)
+        {
+            Dinner currentDinner = dinnerRepository.GetDinner(id);
+
+            if (!currentDinner.IsUserRegistered(User.Identity.Name))
+            {
+                RSVP rsvp = new RSVP() {AttendeeName = User.Identity.Name};
+
+                //TODO
+                //currentDinner.RSVPs = new List<RSVP>();
+                currentDinner.RSVPs.Add(rsvp);
+                dinnerRepository.Save();
+
+            }
+
+            return Content("Thanks - We'll see you there!");
+        }
     }
 }
